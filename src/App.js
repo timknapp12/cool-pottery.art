4727dfefd34c59ac664a8e5e7266a01f43fbd085
Tim Knapp <timknapp12@gmail.com>
2020-04-10 16:34:20 -0600

    add email address to home page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
       Welcome to Cool Pottery. This site is under construction. Come back and
-      see us soon!
+      see us soon! Or send an email to coolpottery.art@gmail.com
     </div>
   );
 }
