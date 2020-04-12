cb876269e15e66fb648c2542b2723df0cd784e3d
Tim Knapp <timknapp12@gmail.com>
2020-04-11 19:06:40 -0600

    add basic tests

--- src/components/common/Flexbox/__test__/Flexbox.test.js
+++ src/components/common/Flexbox/__test__/Flexbox.test.js
@@ -1,8 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Flexbox from "../index";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 
 test("renders with required props without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<Flexbox>test</Flexbox>, div);
+});
+
+test("renders children", () => {
+  const { getByText } = render(
+    <Flexbox>
+      <h1>Hello, World!</h1>
+    </Flexbox>
+  );
+  expect(getByText("Hello, World!")).toBeInTheDocument();
 });
--- src/components/common/Grid/__test__/Grid.test.js
+++ src/components/common/Grid/__test__/Grid.test.js
@@ -1,8 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Grid from "../index";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 
 test("renders with required props without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<Grid>test</Grid>, div);
+});
+
+test("renders children", () => {
+  const { getByText } = render(
+    <Grid>
+      <h1>Cool Pottery</h1>
+    </Grid>
+  );
+  expect(getByText("Cool Pottery")).toBeInTheDocument();
 });
