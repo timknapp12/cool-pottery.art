fe5382066232b2a88018b272f3275c2724dea2f1
Tim Knapp <timknapp12@gmail.com>
2020-08-03 13:23:51 -0600

    add items to nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import Flexbox from "./components/Flexbox";
+import Navbar from "./components/Navbar";
+
+const items = [
+  { name: "Home", href: "/" },
+  { name: "About", href: "/about" },
+  { name: "Gallery", href: "/gallery" },
+];
+
 function App() {
   return (
-    <Flexbox height="150px">
-      Welcome to Cool Pottery. This site is under construction. Come back and
-      see us soon! Or send an email to coolpottery.art@gmail.com
-    </Flexbox>
+    <div>
+      <Navbar items={items} />
+      <Flexbox height="150px">
+        Welcome to Cool Pottery. This site is under construction. Come back and
+        see us soon! Or send an email to coolpottery.art@gmail.com
+      </Flexbox>
+    </div>
   );
 }
 
