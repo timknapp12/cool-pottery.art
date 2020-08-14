ce4cedbc65703658e53b12bfc251d60a8aa62f4f
Tim Knapp <timknapp12@gmail.com>
2020-08-14 16:49:44 -0600

    add tiles to about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,14 +1,33 @@
 import React from "react";
 import styled from "styled-components";
 import Flexbox from "../Flexbox";
-import { StyledText } from "../constants/";
+import { StyledText, white } from "../constants/";
+import Tile from "./Tile";
+import data from "./data";
 
 const Wrapper = styled(Flexbox)`
   padding: 2rem;
+  row-gap: 2rem;
+  height: 100%;
 `;
+
+const TextWrapper = styled(Flexbox)`
+  border: 3px solid ${white};
+  padding: 2rem;
+  border-radius: 8px;
+`;
+
+const ImageWrapper = styled(Flexbox)`
+  flex-direction: column;
+  row-gap: 1rem;
+  @media (min-width: 700px) {
+    flex-direction: row;
+  }
+`;
+
 const About = () => (
   <Wrapper flexDirection="column" justifyContent="space-between" height="100%">
-    <Flexbox width="75%">
+    <TextWrapper width="75%">
       <StyledText>
         Hi. We are just a couple of potters making cool pottery. We draw
         inspiration from many of the classic musicians of the world's history.
@@ -28,8 +47,12 @@ const About = () => (
         first to comment with your zip code. We will then send you a private
         message with calculated shipping (USA only) and we'll go from there :)
       </StyledText>
-    </Flexbox>
-    hello
+    </TextWrapper>
+    <ImageWrapper width="100%" justifyContent="space-around">
+      {data.map((item) => (
+        <Tile key={item.id} {...item} />
+      ))}
+    </ImageWrapper>
   </Wrapper>
 );
 
