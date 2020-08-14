2a39f772924cdb1259a91177e773a7b0ff5b9c09
Tim Knapp <timknapp12@gmail.com>
2020-08-14 16:49:02 -0600

    data for tim and ben

---
+++ src/components/About/data.js
@@ -0,0 +1,23 @@
+import ben from "../../assets/ben.jpg";
+import tim from "../../assets/tim.jpg";
+
+export default [
+  {
+    id: 0,
+    src: ben,
+    alt: "ben",
+    title: "Ben",
+    description:
+      "Co-Founder, CFO, CMO, CTO, Director of Creativity, Lead Technician, Potter",
+    size: "16rem",
+  },
+  {
+    id: 1,
+    src: tim,
+    alt: "tim",
+    title: "Tim",
+    description:
+      "Co-Founder, CEO, COO, CCCP, Instructor, Head of Facilities Maintenance, Potter",
+    size: "16rem",
+  },
+];
